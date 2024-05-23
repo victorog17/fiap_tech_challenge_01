c9d2e88a59dc582560c09f5ca10c1776669e3498
victorog17 <victor.og17@gmail.com>
2024-05-23 13:51:26 -0300

    adicionado looping que cria a lista de links

--- src/listaURLs.py
+++ src/listaURLs.py
@@ -1,4 +1,5 @@
 from anytree import Node, RenderTree
+import json
 
 #udo = Node("Udo")
 #marc = Node("Marc", parent=udo)
@@ -53,10 +54,27 @@ for i in soup.find_all("button"):
             
         
 
-
+lista_link = []
 for pre, fill, node in RenderTree(raiz):
+    lista_link.append(node.name)
     print("%s%s" % (pre, node.name))
 
+print('-'*50)
+print(lista_link)
+print('-'*50)
+for i in lista_link:
+    print(i)
+print('-'*50)
+#lista_link.pop(0)
+new_lista_link = lista_link[2: -1]
+print(new_lista_link)
+print('-'*50)
+print(type(new_lista_link))
+for i in new_lista_link:
+    print(i)
+
+with open("lista_urls.json", mode="w", encoding="utf-8") as events_file:
+        json.dump(new_lista_link, events_file, ensure_ascii=False)
 
 #for i in soup.find_all(class_="footer_content", href=True):
 #    href = i['href']
