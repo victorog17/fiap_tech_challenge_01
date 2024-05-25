b01968eda1fb68cf4143d83c8581b95b589503b3
victorog17 <victor.og17@gmail.com>
2024-05-25 16:30:37 -0300

    atualizacao codigo

--- src/tabelas.py
+++ src/tabelas.py
@@ -1,25 +1,35 @@
 import pandas as pd
-from typing import Dict
-import json as _json
 
-#tabela_extraida = pd.read_csv('Comercio.csv', sep=';')
-#print(tabela_extraida.head())
-#
-#data_dict = tabela_extraida.to_dict(orient="records")
-#print(data_dict)
-
+# Leitura CSVs
 producao = pd.read_csv("Producao.csv", sep=';')
+processa_viniferas = pd.read_csv("ProcessaViniferas.csv", sep='\t')
+processa_americanas = pd.read_csv("ProcessaAmericanas.csv", sep='\t')
+processa_mesa = pd.read_csv("ProcessaMesa.csv", sep='\t')
+processa_semclass = pd.read_csv("ProcessaSemclass.csv", sep='\t')
 comercio = pd.read_csv("Comercio.csv", sep=';')
+imp_vinhos = pd.read_csv("ImpVinhos.csv", sep=';')
+imp_espumantes = pd.read_csv("ImpEspumantes.csv", sep=';')
+imp_frescas = pd.read_csv("ImpFrescas.csv", sep=';')
+imp_passas = pd.read_csv("ImpPassas.csv", sep=';')
+imp_sucos = pd.read_csv("ImpSuco.csv", sep=';')
+exp_vinhos = pd.read_csv("ExpVinho.csv", sep=';')
+exp_espumantes = pd.read_csv("ExpEspumantes.csv", sep=';')
+exp_uva = pd.read_csv("ExpUva.csv", sep=';')
+exp_suco = pd.read_csv("ExpSuco.csv", sep=';')
 
+# Conversao para JSON
 comercio.to_json("Comercio.json")
+processa_viniferas.to_json("ProcessaViniferas.json")
+processa_americanas.to_json("ProcessaAmericanas.json")
+processa_mesa.to_json("ProcessaMesa.json")
+processa_semclass.to_json("ProcessaSemclass.json")
 producao.to_json("Producao.json")
-
-#producao_dict = producao.to_dict(orient="records")
-#comercio_dict = comercio.to_dict(orient="records")
-#print(producao_dict)
-#print('-'*50)
-#print(comercio_dict)
-#
-#lista_tab = [producao_dict, comercio_dict]
-#print('-'*50)
-#print(lista_tab)
+imp_vinhos.to_json("ImpVinhos.json")
+imp_espumantes.to_json("ImpEspumantes.json")
+imp_frescas.to_json("ImpFrescas.json")
+imp_passas.to_json("ImpPassas.json")
+imp_sucos.to_json("ImpSuco.json")
+exp_vinhos.to_json("ExpVinho.json")
+exp_espumantes.to_json("ExpEspumantes.json")
+exp_uva.to_json("ExpUva.json")
+exp_suco.to_json("ExpSuco.json")
--- src/testeAPI.py
+++ src/testeAPI.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Dict
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 import pandas as pd
@@ -9,12 +9,52 @@ import json as _json
 app = FastAPI()
 
 with open("Producao.json", encoding="utf-8") as events_file:
-        data = _json.load(events_file)
+        Producao = _json.load(events_file)
 
 with open("Comercio.json", encoding="utf-8") as events_file:
-        data2 = _json.load(events_file)
+        Comercio = _json.load(events_file)
 
-lista_tab = [data, data2]
+with open("ProcessaViniferas.json", encoding="utf-8") as events_file:
+        ProcessaViniferas = _json.load(events_file)
+
+with open("ProcessaAmericanas.json", encoding="utf-8") as events_file:
+        ProcessaAmericanas = _json.load(events_file)
+
+with open("ProcessaMesa.json", encoding="utf-8") as events_file:
+        ProcessaMesa = _json.load(events_file)
+
+with open("ProcessaSemclass.json", encoding="utf-8") as events_file:
+        ProcessaSemclass = _json.load(events_file)
+
+with open("ImpVinhos.json", encoding="utf-8") as events_file:
+        ImpVinhos = _json.load(events_file)
+
+with open("ImpEspumantes.json", encoding="utf-8") as events_file:
+        ImpEspumantes = _json.load(events_file)
+
+with open("ImpFrescas.json", encoding="utf-8") as events_file:
+        ImpFrescas = _json.load(events_file)
+
+with open("ImpPassas.json", encoding="utf-8") as events_file:
+        ImpPassas = _json.load(events_file)
+
+with open("ImpSuco.json", encoding="utf-8") as events_file:
+        ImpSuco = _json.load(events_file)
+
+with open("ExpVinho.json", encoding="utf-8") as events_file:
+        ExpVinho = _json.load(events_file)
+
+with open("ExpEspumantes.json", encoding="utf-8") as events_file:
+        ExpEspumantes = _json.load(events_file)
+
+with open("ExpUva.json", encoding="utf-8") as events_file:
+        ExpUva = _json.load(events_file)
+
+with open("ExpSuco.json", encoding="utf-8") as events_file:
+        ExpSuco = _json.load(events_file)
+
+lista_tab = [Producao, Comercio, ProcessaViniferas, ProcessaAmericanas, ProcessaMesa, ProcessaSemclass, ImpVinhos
+             , ImpEspumantes, ImpFrescas, ImpPassas, ImpSuco, ExpVinho, ExpEspumantes, ExpUva, ExpSuco]
 
 
 @app.get("/")
@@ -24,23 +64,30 @@ def read_root():
     json_compatible_item_data = jsonable_encoder(tabela)
     return JSONResponse(content=lista_tab)
 
+@app.get("/producao")
+def get_producao(ano: str) -> Dict:
+    data = Producao[ano]
+    return data
 
-#@app.get("/processamento/{name}")
-#def read_processamento(name: str):
-#    if name == 1:
-#        tabela=pd.read_csv("ProcessaViniferas.csv")
-#    tabela=pd.read_csv("Comercio.csv")
-#    #print(tabela.head())
-#    json_compatible_item_data = jsonable_encoder(tabela)
-#    return JSONResponse(content=json_compatible_item_data)
+@app.get("/comercio")
+def get_comercio(ano: str) -> Dict:
+    data = Comercio[ano]
+    return data
 
-
-
-
-
-
-
-
+#@app.get("/processamento", description="Escolha 1: Viníferas, 2: Americanas e híbridas, 3: Uvas de mesa, 4: Sem classificação")
+#def get_processamento(subcategoria: str, ano: str) -> Dict:
+#    try:
+#        if subcategoria == "1":
+#            data = ProcessaViniferas[ano]
+#        elif subcategoria == "2":
+#            data = ProcessaAmericanas[ano]
+#        elif subcategoria == "3":
+#            data = ProcessaMesa[ano]
+#        elif subcategoria == "4":
+#            data = ProcessaSemclass[ano]
+#        return data
+#    except Exception:
+#        return "This is not a option"
 
 
 
