02a4438a0b482334d693e756a92303c7f0cde0ba
victorog17 <victor.og17@gmail.com>
2024-05-25 15:14:35 -0300

    novos testes tabelas

--- src/tabelas.py
+++ src/tabelas.py
@@ -1,4 +1,25 @@
 import pandas as pd
+from typing import Dict
+import json as _json
 
-tabela_extraida = pd.read_csv('..\\src2\\Producao.csv', sep=';')
-print(tabela_extraida.head())
+#tabela_extraida = pd.read_csv('Comercio.csv', sep=';')
+#print(tabela_extraida.head())
+#
+#data_dict = tabela_extraida.to_dict(orient="records")
+#print(data_dict)
+
+producao = pd.read_csv("Producao.csv", sep=';')
+comercio = pd.read_csv("Comercio.csv", sep=';')
+
+comercio.to_json("Comercio.json")
+producao.to_json("Producao.json")
+
+#producao_dict = producao.to_dict(orient="records")
+#comercio_dict = comercio.to_dict(orient="records")
+#print(producao_dict)
+#print('-'*50)
+#print(comercio_dict)
+#
+#lista_tab = [producao_dict, comercio_dict]
+#print('-'*50)
+#print(lista_tab)
--- src/testeAPI.py
+++ src/testeAPI.py
@@ -4,8 +4,17 @@ from fastapi.encoders import jsonable_encoder
 import pandas as pd
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+import json as _json
 
 app = FastAPI()
+
+with open("Producao.json", encoding="utf-8") as events_file:
+        data = _json.load(events_file)
+
+with open("Comercio.json", encoding="utf-8") as events_file:
+        data2 = _json.load(events_file)
+
+lista_tab = [data, data2]
 
 
 @app.get("/")
@@ -13,7 +22,17 @@ def read_root():
     tabela=pd.read_csv("Producao.csv")
     #print(tabela.head())
     json_compatible_item_data = jsonable_encoder(tabela)
-    return JSONResponse(content=json_compatible_item_data)
+    return JSONResponse(content=lista_tab)
+
+
+#@app.get("/processamento/{name}")
+#def read_processamento(name: str):
+#    if name == 1:
+#        tabela=pd.read_csv("ProcessaViniferas.csv")
+#    tabela=pd.read_csv("Comercio.csv")
+#    #print(tabela.head())
+#    json_compatible_item_data = jsonable_encoder(tabela)
+#    return JSONResponse(content=json_compatible_item_data)
 
 
 
